254efe6391aefdf1e3f675414eab797cab29fe66
Vyronas <39767954+Vyronas@users.noreply.github.com>
2019-04-26 16:54:21 -0700

    add TODO

--- Function.cpp
+++ Function.cpp
@@ -9,7 +9,7 @@ Function::Function(string n, string r, string ty, Variable * out, vector<Variabl
 
 string Function::process() {
     if (funcType == "convolution") {
-
+        // TODO: Implement this part to generate real ISA code
     }
     return std::__cxx11::string();
 }
